4456254a8da7beeae8f57e10f0baf5dafae598d9
djterrell <djterrell@sbcglobal.net>
2013-04-09 13:54:12 -0400

    Updated to work with new add.php username and email validation

--- register.php
+++ register.php
@@ -7,6 +7,96 @@ session_start();
 <head>
 <title>Register</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
+<script>
+function validate(form)
+{
+	var Username = frmRegister.loginUsername.value;
+	var Password = frmRegister.loginPassword.value;
+	var ConfirmPassword = frmRegister.loginConfirmPassword.value;
+	var Email = frmRegister.loginEmail.value;
+	var FirstName = frmRegister.loginFirstName.value;
+	var LastName = frmRegister.loginLastName.value;
+	var Address = frmRegister.loginAddress.value;
+	var City = frmRegister.loginCity.value;
+	var State = frmRegister.loginState.value;
+	var Zip = frmRegister.loginZip.value;
+
+
+	if(Username.length < 6)
+	{
+		alert("Your username must be at least 6 characters.");
+		document.frmRegister.loginUsername.focus();
+		return false;
+	}
+
+	if(Password.length < 6)
+	{
+		alert("Your password must be at least 6 characters.");
+		document.frmRegister.loginPassword.focus();
+		return false;
+	}
+
+	if(Password != ConfirmPassword)
+	{
+		alert("Your password and confirmation password did not match.");
+		document.frmRegister.loginPassword.value="";
+		document.frmRegister.loginConfirmPassword.value="";
+		document.frmRegister.loginPassword.focus();
+		return false;
+	}
+
+	if(FirstName.length < 1)
+	{
+		alert("You must enter a first name");
+		document.frmRegister.loginFirstName.focus();
+		return false;
+	}
+	
+	if(LastName.length < 1)
+	{
+		alert("You must enter a last name");
+		document.frmRegister.loginLastName.focus();
+		return false;
+	}
+
+	if(Email.length < 7)
+	{
+		alert("You must enter a valid email address");
+		document.frmRegister.loginEmail.focus();
+		return false;
+	}
+	
+	if(Address.length < 1)
+	{
+		alert("You must enter a valid address");
+		document.frmRegister.loginAddress.focus();
+		return false;
+	}
+
+	if(City.length < 1)
+	{
+		alert("You must enter a valid city");
+		document.frmRegister.loginCity.focus();
+		return false;
+	}
+	
+	if(State.length < 4 && State.length != 2)
+	{
+		alert("You must enter a valid state or state abbreviation");
+		document.frmRegister.loginState.focus();	
+		return false;
+	}
+	
+	if(Zip.length != 5 && Zip.length != 10)
+	{
+		alert("You must enter a valid zip code");
+		document.frmRegister.loginZip.focus();
+		return false;
+	}
+
+	return true;
+}
+</script>
 </head>
 
 <body>
@@ -24,7 +114,7 @@ session_start();
 
 <h3>Register New Account</h3>
 <div id = mainText>
-<form action = "add.php" method = "post">
+<form name = frmRegister action = "add.php" method = "post" onSubmit = "return validate();">
 <table align = "center">
 <tr>
 <td>Username: </td><td><input type="text" name = "loginUsername" ></td>
